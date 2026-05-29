8e9a0a6fef82ef2fd02aeb7f57e1b6f614095384
elernete10075 <timmotim128@gmail.com>
2026-05-29 10:12:40 +0300

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,20 +1,18 @@
 @echo off
-chcp 65001 >nul
-echo Установка XRL-CHAT Bootstrapper для Windows...
+echo Starting XRL-CHAT installation...
 echo ______________________________________
-echo.
 
 set TARGET_DIR=%USERPROFILE%\xrl-chat
 if not exist "%TARGET_DIR%" mkdir "%TARGET_DIR%"
 
-echo [1/3] Скачивание launcher.py...
+echo Downloading launcher...
 powershell -Command "Invoke-WebRequest -Uri 'https://raw.githubusercontent.com/elernete10075/xrl/refs/heads/main/launcher.py' -OutFile '%TARGET_DIR%\launcher.py'"
 
-echo [2/3] Настройка команды запуска...
+echo Setting up launch command...
 set LAUNCHER_BAT=%USERPROFILE%\start-xrl.bat
 echo @echo off > "%LAUNCHER_BAT%"
 echo cd /d "%TARGET_DIR%" >> "%LAUNCHER_BAT%"
 echo python launcher.py >> "%LAUNCHER_BAT%"
 
-echo [3/3] Установка завершена!
+echo Installation finished!
 pause
