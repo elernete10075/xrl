e4df8ba9679a67929bc3e31f85fb0d4092c3579b
elernete10075 <timmotim128@gmail.com>
2026-05-29 12:24:27 +0300

    Update chat.py

--- chat.py
+++ chat.py
@@ -316,7 +316,6 @@ def open_chat(self, stdscr, path):
 
         self.in_chat = False
         stdscr.nodelay(False)
-
     def open_groups(self, stdscr):
         while True:
             parsed = []
